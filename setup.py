b4f0f97aec3f778299b02644ac55df850ee41918
Yuri Shkuro <ys@uber.com>
2016-07-19 18:40:29 -0400

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '2.0.0'
+version = '2.0.1.dev0'
 
 with open('jaeger_client/version.py', 'w') as fp:
     fp.write("__version__ = '%s'\n" % version)
