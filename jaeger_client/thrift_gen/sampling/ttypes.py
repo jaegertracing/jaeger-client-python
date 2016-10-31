a2c08ae4183cd6ebd0848c0600fa1082f6db75b1
Won Jun Jang <wonjun.jangobe@gmail.com>
2016-10-31 12:24:37 -0400

    Update idl (#23)

--- jaeger_client/thrift_gen/sampling/ttypes.py
+++ jaeger_client/thrift_gen/sampling/ttypes.py
@@ -165,12 +165,202 @@ class RateLimitingSamplingStrategy(object):
   def __ne__(self, other):
     return not (self == other)
 
+class OperationSamplingStrategy(object):
+  """
+  Attributes:
+   - operation
+   - probabilisticSampling
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.STRING, 'operation', None, None, ), # 1
+    (2, TType.STRUCT, 'probabilisticSampling', (ProbabilisticSamplingStrategy, ProbabilisticSamplingStrategy.thrift_spec), None, ), # 2
+  )
+
+  def __init__(self, operation=None, probabilisticSampling=None,):
+    self.operation = operation
+    self.probabilisticSampling = probabilisticSampling
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.STRING:
+          self.operation = iprot.readString()
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.STRUCT:
+          self.probabilisticSampling = ProbabilisticSamplingStrategy()
+          self.probabilisticSampling.read(iprot)
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('OperationSamplingStrategy')
+    if self.operation is not None:
+      oprot.writeFieldBegin('operation', TType.STRING, 1)
+      oprot.writeString(self.operation)
+      oprot.writeFieldEnd()
+    if self.probabilisticSampling is not None:
+      oprot.writeFieldBegin('probabilisticSampling', TType.STRUCT, 2)
+      self.probabilisticSampling.write(oprot)
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    if self.operation is None:
+      raise TProtocol.TProtocolException(message='Required field operation is unset!')
+    if self.probabilisticSampling is None:
+      raise TProtocol.TProtocolException(message='Required field probabilisticSampling is unset!')
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.operation)
+    value = (value * 31) ^ hash(self.probabilisticSampling)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
+class PerOperationSamplingStrategies(object):
+  """
+  Attributes:
+   - defaultSamplingProbability
+   - defaultLowerBoundTracesPerSecond
+   - perOperationStrategies
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.DOUBLE, 'defaultSamplingProbability', None, None, ), # 1
+    (2, TType.DOUBLE, 'defaultLowerBoundTracesPerSecond', None, None, ), # 2
+    (3, TType.LIST, 'perOperationStrategies', (TType.STRUCT,(OperationSamplingStrategy, OperationSamplingStrategy.thrift_spec)), None, ), # 3
+  )
+
+  def __init__(self, defaultSamplingProbability=None, defaultLowerBoundTracesPerSecond=None, perOperationStrategies=None,):
+    self.defaultSamplingProbability = defaultSamplingProbability
+    self.defaultLowerBoundTracesPerSecond = defaultLowerBoundTracesPerSecond
+    self.perOperationStrategies = perOperationStrategies
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.DOUBLE:
+          self.defaultSamplingProbability = iprot.readDouble()
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.DOUBLE:
+          self.defaultLowerBoundTracesPerSecond = iprot.readDouble()
+        else:
+          iprot.skip(ftype)
+      elif fid == 3:
+        if ftype == TType.LIST:
+          self.perOperationStrategies = []
+          (_etype3, _size0) = iprot.readListBegin()
+          for _i4 in xrange(_size0):
+            _elem5 = OperationSamplingStrategy()
+            _elem5.read(iprot)
+            self.perOperationStrategies.append(_elem5)
+          iprot.readListEnd()
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('PerOperationSamplingStrategies')
+    if self.defaultSamplingProbability is not None:
+      oprot.writeFieldBegin('defaultSamplingProbability', TType.DOUBLE, 1)
+      oprot.writeDouble(self.defaultSamplingProbability)
+      oprot.writeFieldEnd()
+    if self.defaultLowerBoundTracesPerSecond is not None:
+      oprot.writeFieldBegin('defaultLowerBoundTracesPerSecond', TType.DOUBLE, 2)
+      oprot.writeDouble(self.defaultLowerBoundTracesPerSecond)
+      oprot.writeFieldEnd()
+    if self.perOperationStrategies is not None:
+      oprot.writeFieldBegin('perOperationStrategies', TType.LIST, 3)
+      oprot.writeListBegin(TType.STRUCT, len(self.perOperationStrategies))
+      for iter6 in self.perOperationStrategies:
+        iter6.write(oprot)
+      oprot.writeListEnd()
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    if self.defaultSamplingProbability is None:
+      raise TProtocol.TProtocolException(message='Required field defaultSamplingProbability is unset!')
+    if self.defaultLowerBoundTracesPerSecond is None:
+      raise TProtocol.TProtocolException(message='Required field defaultLowerBoundTracesPerSecond is unset!')
+    if self.perOperationStrategies is None:
+      raise TProtocol.TProtocolException(message='Required field perOperationStrategies is unset!')
+    return
+
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.defaultSamplingProbability)
+    value = (value * 31) ^ hash(self.defaultLowerBoundTracesPerSecond)
+    value = (value * 31) ^ hash(self.perOperationStrategies)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
 class SamplingStrategyResponse(object):
   """
   Attributes:
    - strategyType
    - probabilisticSampling
    - rateLimitingSampling
+   - operationSampling
   """
 
   thrift_spec = (
@@ -178,12 +368,14 @@ class SamplingStrategyResponse(object):
     (1, TType.I32, 'strategyType', None, None, ), # 1
     (2, TType.STRUCT, 'probabilisticSampling', (ProbabilisticSamplingStrategy, ProbabilisticSamplingStrategy.thrift_spec), None, ), # 2
     (3, TType.STRUCT, 'rateLimitingSampling', (RateLimitingSamplingStrategy, RateLimitingSamplingStrategy.thrift_spec), None, ), # 3
+    (4, TType.STRUCT, 'operationSampling', (PerOperationSamplingStrategies, PerOperationSamplingStrategies.thrift_spec), None, ), # 4
   )
 
-  def __init__(self, strategyType=None, probabilisticSampling=None, rateLimitingSampling=None,):
+  def __init__(self, strategyType=None, probabilisticSampling=None, rateLimitingSampling=None, operationSampling=None,):
     self.strategyType = strategyType
     self.probabilisticSampling = probabilisticSampling
     self.rateLimitingSampling = rateLimitingSampling
+    self.operationSampling = operationSampling
 
   def read(self, iprot):
     if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
@@ -211,6 +403,12 @@ class SamplingStrategyResponse(object):
           self.rateLimitingSampling.read(iprot)
         else:
           iprot.skip(ftype)
+      elif fid == 4:
+        if ftype == TType.STRUCT:
+          self.operationSampling = PerOperationSamplingStrategies()
+          self.operationSampling.read(iprot)
+        else:
+          iprot.skip(ftype)
       else:
         iprot.skip(ftype)
       iprot.readFieldEnd()
@@ -233,6 +431,10 @@ class SamplingStrategyResponse(object):
       oprot.writeFieldBegin('rateLimitingSampling', TType.STRUCT, 3)
       self.rateLimitingSampling.write(oprot)
       oprot.writeFieldEnd()
+    if self.operationSampling is not None:
+      oprot.writeFieldBegin('operationSampling', TType.STRUCT, 4)
+      self.operationSampling.write(oprot)
+      oprot.writeFieldEnd()
     oprot.writeFieldStop()
     oprot.writeStructEnd()
 
@@ -247,6 +449,7 @@ class SamplingStrategyResponse(object):
     value = (value * 31) ^ hash(self.strategyType)
     value = (value * 31) ^ hash(self.probabilisticSampling)
     value = (value * 31) ^ hash(self.rateLimitingSampling)
+    value = (value * 31) ^ hash(self.operationSampling)
     return value
 
   def __repr__(self):
