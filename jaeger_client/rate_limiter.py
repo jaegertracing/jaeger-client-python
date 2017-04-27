a6c15c0d9509c8abfa39ab47cc977e285a0316bf
Won Jun Jang <wjang@uber.com>
2017-04-27 15:44:50 -0400

    Rafactor our RateLimiter from sampler.py

---
+++ jaeger_client/rate_limiter.py
@@ -0,0 +1,38 @@
+class RateLimiter(object):
+    """
+    RateLimiter is based on leaky bucket algorithm, formulated in terms
+    of a credits balance that is replenished every time check_credit()
+    method is called (tick) by the amount proportional to the time
+    elapsed since the last tick, up to max of credits_per_second. A call
+    to check_credit() takes a cost of an item we want to pay with the
+    balance. If the balance exceeds the cost of the item, the item is
+    "purchased" and the balance reduced, indicated by returned value of
+    true. Otherwise the balance is unchanged and return false.
+
+    This can be used to limit a rate of messages emitted by a service
+    by instantiating the Rate Limiter with the max number of messages a
+    service is allowed to emit per second, and calling check_credit(1.0)
+    for each message to determine if the message is within the rate limit.
+
+    It can also be used to limit the rate of traffic in bytes, by setting
+    credits_per_second to desired throughput as bytes/second, and calling
+    check_credit() with the actual message size.
+    """
+
+    def __init__(self, credits_per_second=10.0, max_balance=10.0):
+        self.credits_per_second = credits_per_second
+        self.max_balance = max_balance
+        self.balance = self.max_balance
+        self.last_tick = self.timestamp()
+
+    def check_credit(self, item_cost=1.0):
+        current_time = self.timestamp()
+        elapsed_time = current_time - self.last_tick
+        self.last_tick = current_time
+        self.balance += elapsed_time * self.credits_per_second
+        if self.balance > self.max_balance:
+            self.balance = self.max_balance
+        if self.balance >= item_cost:
+            self.balance -= item_cost
+            return True
+        return False
