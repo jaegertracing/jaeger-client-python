cfe390c5ace68a94ceb49caabd4fc889dc0e861b
Alexander <alexander.demidko@stitchfix.com>
2017-05-02 13:47:16 -0700

    setup.py housekeeping

--- setup.py
+++ setup.py
@@ -33,13 +33,13 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
     ],
     install_requires=[
         'future',
         'futures',
         'threadloop>=1,<2',
-        # we want thrift>=0.9.2.post1,<0.9.3, but we let the users pin to that
-        'thrift',
+        'thrift>=0.10.0',
         'tornado>=4.3,<5',
         'opentracing>=1.2.2,<1.3',
     ],
