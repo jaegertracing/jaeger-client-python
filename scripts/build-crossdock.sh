45a31d58008d2f0c71208763dceebb060b000d0d
Yuri Shkuro <ys@uber.com>
2017-11-22 15:29:04 -0500

    Fix damn it
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- scripts/build-crossdock.sh
+++ scripts/build-crossdock.sh
@@ -4,7 +4,7 @@ set -e
 
 make crossdock
 
-export REPO=jaegertracing/xdock-python
+export REPO=jaegertracing/xdock-py
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 export TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo "${BRANCH///}"; fi`
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, REPO=$REPO, PR=$PR, BRANCH=$BRANCH, TAG=$TAG"
