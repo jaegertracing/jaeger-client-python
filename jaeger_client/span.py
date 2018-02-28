3ad5dbd156832b665de8688f3cd929469e7a0baf
Debosmit Ray <debo@uber.com>
2018-02-27 22:04:06 -0800

    encode to utf-8 makes it blow up later
    Signed-off-by: Debosmit Ray <debo@uber.com>

--- jaeger_client/span.py
+++ jaeger_client/span.py
@@ -114,8 +114,8 @@ class Span(opentracing.Span):
         if self.is_sampled():
             logs = {
                 'event': 'baggage',
-                'key': key,
-                'value': value,
+                'key': key.encode('utf-8'),
+                'value': value.encode('utf-8'),
             }
             if prev_value:
                 # TODO add metric for this
