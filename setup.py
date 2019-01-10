93b5ea505ed1d26b283c449c4f55132b2562f1b6
Yuri Shkuro <ys@uber.com>
2019-01-10 14:46:53 -0500

    Pin test module
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
             'coverage<4.4',  # can remove after https://bitbucket.org/ned/coveragepy/issues/581/44b1-44-breaking-in-ci
             'pytest-timeout==1.3.1',
             'pytest-tornado',
-            'pytest-benchmark[histogram]>=3.0.0rc1',
+            # pin <3.2 as otherwise it requires pytest>=3.8
+            'pytest-benchmark[histogram]>=3.0.0rc1,<3.2',
             'pytest-localserver',
             'flake8',
             'flake8-quotes',
