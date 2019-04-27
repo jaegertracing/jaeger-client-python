b4e2d1098d6e2d9d215b0565f2d3241437f4b300
Yuri Shkuro <ys@uber.com>
2019-04-27 16:47:21 -0400

    Bump OT to 2.1 and remove test override
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -37,20 +37,3 @@ class APITest(unittest.TestCase, APICompatibilityCheckMixin):
 
     def is_parent(self, parent, span):
         return span.parent_id == getattr(parent, 'span_id', None)
-
-    # NOTE: this overrides a method defined in ApiComaptibilityCheckMixin in
-    # order to add the scope.close() call, which prevents the tracer from
-    # leaving an active scope in thread local storage
-    # TODO: remove after switching to opentracing-python 2.1.0, issue for
-    # tracking the release:
-    # https://github.com/opentracing/opentracing-python/issues/119
-    def test_start_active_span(self):
-        # the first usage returns a `Scope` that wraps a root `Span`
-        tracer = self.tracer()
-        scope = tracer.start_active_span('Fry')
-
-        assert scope.span is not None
-        if self.check_scope_manager():
-            assert self.is_parent(None, scope.span)
-
-        scope.close()
