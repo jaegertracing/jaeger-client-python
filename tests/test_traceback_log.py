4e4aa1ca106308568a42582d763dce1d7037aba9
Qi Qi <piano.keys0@gmail.com>
2019-06-12 09:38:44 -0400

    Automatically catch exception using span in tests
    Instead of manually logging an exception, use the context manager
    properties of the span to automatically catch and log exceptions
    when testing for traceback reporting behavior.
    
    Signed-off-by: Qi Qi <piano.keys0@gmail.com>

--- tests/test_traceback_log.py
+++ tests/test_traceback_log.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import mock
-import sys
 from jaeger_client import Span, SpanContext
 
 
@@ -25,39 +24,31 @@ def test_traceback_full():
     span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
 
     try:
-        raise ValueError('Something unexpected happened!')
-
+        with span:
+            raise ValueError('Something unexpected happened!')
     except ValueError:
-        traceback = sys.exc_info()[2]
-        span.log_kv({'stack': traceback})
         fields_dict = {field.key: field.vStr for field in span.logs[0].fields}
-
         assert 'stack' in fields_dict
         stack_message = fields_dict['stack']
         stack_message_lines = stack_message.splitlines()
-        assert stack_message_lines[0] == 'Traceback (most recent call last):'
-        assert stack_message_lines[1].startswith('  File ')
-        assert stack_message_lines[2] == \
+        assert len(stack_message_lines) == 2
+        assert stack_message_lines[0].startswith('  File ')
+        assert stack_message_lines[1] == \
             "    raise ValueError('Something unexpected happened!')"
-        assert stack_message_lines[3] == \
-            'ValueError: Something unexpected happened!'
 
 
 def test_traceback_cut():
     """Test that a traceback is cut off at max_tag_value_length"""
     mock_tracer = mock.MagicMock()
-    mock_tracer.max_tag_value_length = 15
+    mock_tracer.max_tag_value_length = 5
     context = SpanContext(trace_id=1, span_id=2, parent_id=None, flags=1)
     span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
 
     try:
-        raise ValueError('Something unexpected happened!')
-
+        with span:
+            raise ValueError('Something unexpected happened!')
     except ValueError:
-        traceback = sys.exc_info()[2]
-        span.log_kv({'stack': traceback})
         fields_dict = {field.key: field.vStr for field in span.logs[0].fields}
-
         assert 'stack' in fields_dict
         stack_message = fields_dict['stack']
-        assert stack_message == 'Traceback (most'
+        assert stack_message == '  Fil'
