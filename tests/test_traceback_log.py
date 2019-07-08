724409d200c33c923164039cf16bdbf72a2da54a
Qi Qi <piano.keys0@gmail.com>
2019-07-08 11:58:47 -0400

    Fix traceback test
    Signed-off-by: Qi Qi <piano.keys0@gmail.com>

--- tests/test_traceback_log.py
+++ tests/test_traceback_log.py
@@ -16,11 +16,10 @@ import mock
 from jaeger_client import Span, SpanContext
 
 
-def test_traceback_full():
+def test_traceback():
     """Test that a traceback is logged with both location and message"""
     mock_tracer = mock.MagicMock()
     mock_tracer.max_tag_value_length = 300
-    mock_tracer.max_traceback_length = 300
     context = SpanContext(trace_id=1, span_id=2, parent_id=None, flags=1)
     span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
 
@@ -36,21 +35,3 @@ def test_traceback_full():
         assert stack_message_lines[0].startswith('  File ')
         assert stack_message_lines[1] == \
             "    raise ValueError('Something unexpected happened!')"
-
-
-def test_traceback_cut():
-    """Test that a traceback is cut off at max_tag_value_length"""
-    mock_tracer = mock.MagicMock()
-    mock_tracer.max_tag_value_length = 300
-    mock_tracer.max_traceback_length = 5
-    context = SpanContext(trace_id=1, span_id=2, parent_id=None, flags=1)
-    span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
-
-    try:
-        with span:
-            raise ValueError('Something unexpected happened!')
-    except ValueError:
-        fields_dict = {field.key: field.vStr for field in span.logs[0].fields}
-        assert 'stack' in fields_dict
-        stack_message = fields_dict['stack']
-        assert stack_message == '  Fil'
