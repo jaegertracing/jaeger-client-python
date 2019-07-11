7d1c8730dc1713617baaeb3c208d9278ce6e77f1
Qi Qi <piano.keys0@gmail.com>
2019-07-11 11:14:45 -0400

    Add traceback length to config, spans, etc
    Signed-off-by: Qi Qi <piano.keys0@gmail.com>

--- jaeger_client/tracer.py
+++ jaeger_client/tracer.py
@@ -51,6 +51,7 @@ class Tracer(opentracing.Tracer):
         one_span_per_rpc=False, extra_codecs=None,
         tags=None,
         max_tag_value_length=constants.MAX_TAG_VALUE_LENGTH,
+        max_traceback_length=constants.MAX_TRACEBACK_LENGTH,
         throttler=None,
         scope_manager=None,
     ):
@@ -63,6 +64,7 @@ class Tracer(opentracing.Tracer):
         self.debug_id_header = debug_id_header
         self.one_span_per_rpc = one_span_per_rpc
         self.max_tag_value_length = max_tag_value_length
+        self.max_traceback_length = max_traceback_length
         self.max_trace_id_bits = constants._max_trace_id_bits if generate_128bit_trace_id \
             else constants._max_id_bits
         self.codecs = {
--- tests/test_traceback_log.py
+++ tests/test_traceback_log.py
@@ -20,6 +20,7 @@ def test_traceback():
     """Test that a traceback is logged with both location and message"""
     mock_tracer = mock.MagicMock()
     mock_tracer.max_tag_value_length = 300
+    mock_tracer.max_traceback_length = 300
     context = SpanContext(trace_id=1, span_id=2, parent_id=None, flags=1)
     span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
 
@@ -35,3 +36,21 @@ def test_traceback():
         assert stack_message_lines[0].startswith('  File ')
         assert stack_message_lines[1] == \
             "    raise ValueError('Something unexpected happened!')"
+
+
+def test_traceback_cut():
+    """Test that a traceback is cut off at max_tag_value_length"""
+    mock_tracer = mock.MagicMock()
+    mock_tracer.max_tag_value_length = 300
+    mock_tracer.max_traceback_length = 5
+    context = SpanContext(trace_id=1, span_id=2, parent_id=None, flags=1)
+    span = Span(context=context, operation_name='traceback_test', tracer=mock_tracer)
+
+    try:
+        with span:
+            raise ValueError('Something unexpected happened!')
+    except ValueError:
+        fields_dict = {field.key: field.vStr for field in span.logs[0].fields}
+        assert 'stack' in fields_dict
+        stack_message = fields_dict['stack']
+        assert stack_message == '  Fil'
