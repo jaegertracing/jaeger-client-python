3be453d046f01c08e442318877d8db06e1b2235a
Ivan Filenko <ivan.filenko@protonmail.com>
2019-10-01 03:04:23 +0300

    Run crossdock with all-in-one instead of Cassandra
    Signed-off-by: Ivan Filenko <ivan.filenko@protonmail.com>

--- crossdock/rules.mk
+++ crossdock/rules.mk
@@ -11,28 +11,24 @@ ${TRACETEST_THRIFT}:
 	git submodule update --init idl
 
 .PHONY: docker
-docker: clean-compile crossdock-download-jaeger
+docker: clean-compile
 	docker build -f crossdock/Dockerfile -t jaeger-client-python .
 
 .PHONY: crossdock
-crossdock: ${TRACETEST_THRIFT} crossdock-download-jaeger
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) kill python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) rm -f python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) build python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) run crossdock
+crossdock: ${TRACETEST_THRIFT}
+	docker-compose -f $(XDOCK_YAML) kill python
+	docker-compose -f $(XDOCK_YAML) rm -f python
+	docker-compose -f $(XDOCK_YAML) build python
+	docker-compose -f $(XDOCK_YAML) run crossdock
 
 .PHONY: crossdock-fresh
-crossdock-fresh: ${TRACETEST_THRIFT} crossdock-download-jaeger
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) kill
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) rm --force
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) pull
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) build
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) run crossdock
+crossdock-fresh: ${TRACETEST_THRIFT}
+	docker-compose -f $(XDOCK_YAML) kill
+	docker-compose -f $(XDOCK_YAML) rm --force
+	docker-compose -f $(XDOCK_YAML) pull
+	docker-compose -f $(XDOCK_YAML) build
+	docker-compose -f $(XDOCK_YAML) run crossdock
 
-.PHONY: crossdock-logs crossdock-download-jaeger
+.PHONY: crossdock-logs
 crossdock-logs:
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) logs
-
-.PHONY: crossdock-download-jaeger
-crossdock-download-jaeger:
-	curl -o $(XDOCK_JAEGER_YAML) $(JAEGER_COMPOSE_URL)
+	docker-compose -f $(XDOCK_YAML) logs
