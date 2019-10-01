eb0c467f072cb37a2f19cb373da24072e6d982f0
Bhavin Gandhi <bhavin192@users.noreply.github.com>
2019-10-01 09:21:57 +0530

    Run crossdock with all-in-one (#255)
    - Applies same changes as of
      https://github.com/jaegertracing/jaeger-client-go/pull/420
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- crossdock/rules.mk
+++ crossdock/rules.mk
@@ -1,7 +1,5 @@
 XDOCK_YAML=crossdock/docker-compose.yml
 TRACETEST_THRIFT=idl/thrift/crossdock/tracetest.thrift
-JAEGER_COMPOSE_URL=https://raw.githubusercontent.com/jaegertracing/jaeger/master/crossdock/jaeger-docker-compose.yml
-XDOCK_JAEGER_YAML=crossdock/jaeger-docker-compose.yml
 
 .PHONY: clean-compile
 clean-compile:
@@ -11,28 +9,24 @@ ${TRACETEST_THRIFT}:
 	git submodule update --init idl
 
 .PHONY: docker
-docker: clean-compile crossdock-download-jaeger
+docker: clean-compile
 	docker build -f crossdock/Dockerfile -t jaeger-client-python .
 
 .PHONY: crossdock
-crossdock: ${TRACETEST_THRIFT} crossdock-download-jaeger
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) kill python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) rm -f python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) build python
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) run crossdock
+crossdock: ${TRACETEST_THRIFT}
+	docker-compose -f $(XDOCK_YAML) kill python
+	docker-compose -f $(XDOCK_YAML) rm -f python
+	docker-compose -f $(XDOCK_YAML) build python
+	docker-compose -f $(XDOCK_YAML) run crossdock
 
 .PHONY: crossdock-fresh
-crossdock-fresh: ${TRACETEST_THRIFT} crossdock-download-jaeger
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) kill
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) rm --force
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) pull
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) build
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) run crossdock
+crossdock-fresh: ${TRACETEST_THRIFT}
+	docker-compose -f $(XDOCK_YAML) kill
+	docker-compose -f $(XDOCK_YAML) rm --force
+	docker-compose -f $(XDOCK_YAML) pull
+	docker-compose -f $(XDOCK_YAML) build
+	docker-compose -f $(XDOCK_YAML) run crossdock
 
-.PHONY: crossdock-logs crossdock-download-jaeger
+.PHONY: crossdock-logs
 crossdock-logs:
-	docker-compose -f $(XDOCK_YAML) -f $(XDOCK_JAEGER_YAML) logs
-
-.PHONY: crossdock-download-jaeger
-crossdock-download-jaeger:
-	curl -o $(XDOCK_JAEGER_YAML) $(JAEGER_COMPOSE_URL)
+	docker-compose -f $(XDOCK_YAML) logs
