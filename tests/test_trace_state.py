e95d7e184215ac553d3c9078ebfab55d7cec9255
Craig Holden <Tecktron@users.noreply.github.com>
2020-09-30 15:54:41 -0400

    Linted tests Signed-off-by: Craig Holden <Tecktron@users.noreply.github.com>

--- tests/test_trace_state.py
+++ tests/test_trace_state.py
@@ -27,25 +27,27 @@ def test_basic_init():
 
 
 def test_add_trace():
-    trace_state = TraceState(operation_name="foo", trace_id="0123456789abcdef")
-    trace_state.add("bar", "testing")
-    trace_state.add("novalue")
+    trace_state = TraceState(operation_name='foo', trace_id='0123456789abcdef')
+    trace_state.add('bar', 'testing')
+    trace_state.add('novalue')
     trace_state.add(None)
-    assert trace_state._trace_state == OrderedDict([("bar", "testing"), ("foo", "0123456789abcdef")])
-    trace_state.add("foo")
-    assert trace_state._trace_state == OrderedDict([("foo", "0123456789abcdef"), ("bar", "testing")])
+    assert trace_state._trace_state == OrderedDict(
+        [('bar', 'testing'), ('foo', '0123456789abcdef')])
+    trace_state.add('foo')
+    assert trace_state._trace_state == OrderedDict(
+        [('foo', '0123456789abcdef'), ('bar', 'testing')])
 
 
 def test_add_trace_with_encoder():
     def encoder(value):
-        return six.ensure_str(base64.urlsafe_b64encode(value.encode('utf-8'))).rstrip("=")
+        return six.ensure_str(base64.urlsafe_b64encode(value.encode('utf-8'))).rstrip('=')
 
-    encoded = encoder("0123456789abcdef"), encoder("testing")
+    encoded = encoder('0123456789abcdef'), encoder('testing')
 
-    trace_state = TraceState(operation_name="foo", trace_id="0123456789abcdef", encoder=encoder)
-    trace_state.add("bar", "testing")
-    assert trace_state._trace_state["foo"] == encoded[0]
-    assert trace_state._trace_state["bar"] == encoded[1]
+    trace_state = TraceState(operation_name='foo', trace_id='0123456789abcdef', encoder=encoder)
+    trace_state.add('bar', 'testing')
+    assert trace_state._trace_state['foo'] == encoded[0]
+    assert trace_state._trace_state['bar'] == encoded[1]
 
 
 def test_set_encoder():
@@ -60,31 +62,31 @@ def test_set_encoder():
 
 def test_parse_from_header():
     trace_state = TraceState()
-    header_data = "foo=0123456789abcdef,bar=testing,test=qwerty,garbage"
+    header_data = 'foo=0123456789abcdef,bar=testing,test=qwerty,garbage'
     trace_state.parse_from_header(header_data)
     assert trace_state._trace_state == OrderedDict(
-        [("foo", "0123456789abcdef"), ("bar", "testing"), ("test", "qwerty")])
+        [('foo', '0123456789abcdef'), ('bar', 'testing'), ('test', 'qwerty')])
 
 
 def test_get_formatted_header():
     trace_state = TraceState()
-    assert trace_state.get_formatted_header() == ""
-    trace_state.add("foo", "0123456789abcdef")
-    trace_state.add("bar", "testing")
-    trace_state.add("test", "qwerty")
+    assert trace_state.get_formatted_header() == ''
+    trace_state.add('foo', '0123456789abcdef')
+    trace_state.add('bar', 'testing')
+    trace_state.add('test', 'qwerty')
     headers = trace_state.get_formatted_header(url_parse=False)
     formatted_headers = urllib_parse.quote(headers)
-    assert headers == "test=qwerty,bar=testing,foo=0123456789abcdef"
+    assert headers == 'test=qwerty,bar=testing,foo=0123456789abcdef'
     headers = trace_state.get_formatted_header()
     assert headers == formatted_headers
 
 
 def test_init_with_data():
     trace_state = TraceState(
-        operation_name="foo",
-        trace_id="0123456789abcdef",
-        header_values="bar=testing,test=qwerty,garbage"
+        operation_name='foo',
+        trace_id='0123456789abcdef',
+        header_values='bar=testing,test=qwerty,garbage'
     )
     assert len(trace_state._trace_state) == 3
     assert trace_state._trace_state == OrderedDict(
-        [("foo", "0123456789abcdef"), ("bar", "testing"), ("test", "qwerty")])
+        [('foo', '0123456789abcdef'), ('bar', 'testing'), ('test', 'qwerty')])
