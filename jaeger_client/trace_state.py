eab9e606e8bc65d472e6ee6b6b9d15e9857f6ca3
Craig Holden <git@tecktron.net>
2020-09-30 11:54:48 -0400

    Added TraceState class and tests. The TraceState class is used to handle the trace state for the W3C Trace Context. Signed-off-by: Craig Holden <Tecktron@users.noreply.github.com>

---
+++ jaeger_client/trace_state.py
@@ -0,0 +1,94 @@
+from __future__ import absolute_import
+
+from collections import OrderedDict
+
+import six
+import sys
+from six.moves import urllib_parse
+
+
+class TraceState(object):
+    __slots__ = ["_trace_state", "_encoder"]
+
+    def __init__(self,
+                 operation_name=None,
+                 trace_id=None,
+                 header_values=None,
+                 encoder=None
+        ):
+        self._trace_state = OrderedDict()
+        self._encoder = encoder
+        if header_values:
+            self.parse_from_header(header_values)
+        if operation_name and trace_id:
+            self.add(operation_name, trace_id)
+
+    def set_encoder(self, encoder=None):
+        self._encoder = encoder
+
+    def parse_from_header(self, value):
+        states = six.ensure_str(urllib_parse.unquote(value)).split(',')
+        states.reverse()
+        for state in states:
+            if '=' in state:
+                key, value = state.split('=', 1)
+                self.add(key, value, skip_encode=True)
+
+    def add(self, key, value=None, skip_encode=False):
+        if not key:
+            return
+        if not value:
+            value = self._trace_state.get(key, None)
+            skip_encode = True
+            if not value:
+                return
+
+        if self._encoder and skip_encode is False:
+            value = self._encoder(value)
+
+        if six.PY2:
+            key = unicode(key)
+            value = unicode(value)
+        else:
+            key = str(key)
+            value = str(value)
+
+        key = six.ensure_str(key)
+        value = six.ensure_str(value)
+
+        if sys.version_info >= (3, 2, 0):
+            self._trace_state[key] = value
+            self._trace_state.move_to_end(key, last=False)
+        else:
+            # No so graceful in older versions
+            root = self._trace_state._OrderedDict__root  # noqa
+            first = root[1]
+
+            if key in self._trace_state:
+                link = self._trace_state._OrderedDict__map[key]  # noqa
+                link_prev, link_next, _ = link
+                link_prev[1] = link_next
+                link_next[0] = link_prev
+                link[0] = root
+                link[1] = first
+                root[1] = first[0] = link
+            else:
+                root[1] = first[0] = self._trace_state._OrderedDict__map[key] = [root, first, key]  # no_qa
+                dict.__setitem__(self._trace_state, key, value)
+
+    def get_formatted_header(self, url_parse=True):
+        traces = []
+        if not self._trace_state:
+            return ""
+        for key, value in six.iteritems(self._trace_state):
+            traces.append("{}={}".format(key, value))
+
+        header_traces = ','.join(traces)
+        if url_parse is True:
+            if six.PY2:
+                header_traces = urllib_parse.quote(header_traces.encode('utf-8'))
+            else:
+                header_traces = urllib_parse.quote(header_traces)
+
+        return header_traces
+
