c8d43f034bbde158966b583c03e976cf1d6752bc
obataku <19821199+obataku@users.noreply.github.com>
2020-12-01 04:48:41 -0500

    Ensure Tracer.close() properly flushes finished but unsent spans
    * assigning to Reporter._stopped in _flush() instead of close()
    avoids dropping those spans passed to report_span just _prior_ to
    Reporter.close() for which _report_span_from_ioloop will execute only
    *afterwards*; this way those scheduled callbacks will get a chance to
    run before _flush stops the reporter and those pending spans make it
    into the queue before the reporter shuts down.
    
    Signed-off-by: obataku <19821199+obataku@users.noreply.github.com>

--- jaeger_client/reporter.py
+++ jaeger_client/reporter.py
@@ -215,13 +215,13 @@ class Reporter(NullReporter):
         Ensure that all spans from the queue are submitted.
         Returns Future that will be completed once the queue is empty.
         """
-        with self.stop_lock:
-            self.stopped = True
-
         return ioloop_util.submit(self._flush, io_loop=self.io_loop)
 
     @tornado.gen.coroutine
     def _flush(self):
+        # stopping here ensures we don't lose spans from pending _report_span_from_ioloop callbacks
+        with self.stop_lock:
+            self.stopped = True
         yield self.queue.put(self.stop)
         yield self.queue.join()
 
